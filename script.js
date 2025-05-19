212e9557d00dc51f6adb9bb7c76e385abb9f8b27
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-19 09:54:21 +0200

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+// * script mobile menu
+document.getElementById('hamburgerMenu').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.getElementById('navLinks').classList.toggle('active');
+});
